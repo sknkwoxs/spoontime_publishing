712117f2d89ff8c1e5f5eb670aeb892445063905
seankim1111 <yongsun199@gmail.com>
2022-08-18 10:10:26 +0900

    load svg files

--- src/pages/Recipe/AddMenu/AddMenu.js
+++ src/pages/Recipe/AddMenu/AddMenu.js
@@ -45,58 +45,11 @@ export default function AddMenu() {
               <p className="Font20sb mb-2">한우가지로메인죽</p>
               <div className="Font12 flex text-GreyScale-grey02">
                 <div className="flex items-center gap-1 mr-2">
-                  <svg
-                    width="14"
-                    height="12"
-                    viewBox="0 0 14 12"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M11.8555 4.80779C11.8034 4.80779 11.7602 4.83014 11.7094 4.83573C11.2001 2.6385 9.29851 1 7.01461 1C4.73071 1 2.82534 2.64129 2.31724 4.84132C2.25881 4.83294 2.20546 4.8064 2.14449 4.8064C1.51318 4.8064 1 5.31205 1 5.93505C1 6.55804 1.51318 7.0637 2.14449 7.0637C2.19276 7.0637 2.23468 7.04135 2.28168 7.03576C2.74405 9.30004 4.68625 11 7.01461 11C9.34297 11 11.2814 9.30284 11.7463 7.04274C11.7844 7.04693 11.8161 7.06509 11.8555 7.06509C12.4868 7.06509 13 6.55944 13 5.93644C13 5.31345 12.4881 4.80779 11.8555 4.80779Z"
-                      fill="white"
-                      stroke="#555555"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                    <path
-                      d="M6 4.50077C6 4.77743 5.77554 5 5.5 5C5.22446 5 5 4.77743 5 4.50077C5 4.22411 5.22446 4 5.5 4C5.77554 4 6 4.22257 6 4.50077Z"
-                      fill="#555555"
-                    />
-                    <path
-                      d="M9 4.50077C9 4.77743 8.77554 5 8.5 5C8.22446 5 8 4.77743 8 4.50077C8 4.22411 8.22446 4 8.5 4C8.77554 4 9 4.22257 9 4.50077Z"
-                      fill="#555555"
-                    />
-                    <path
-                      d="M9 7C7.89504 8.33333 6.10496 8.33333 5 7"
-                      stroke="#555555"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
+                  <img src="/images/svgIcons/baby.svg" alt="baby" />
                   <p>중기</p>
                 </div>
                 <div className="flex items-center gap-1 mr-2">
-                  <svg
-                    width="12"
-                    height="12"
-                    viewBox="0 0 12 12"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M6 11C8.76142 11 11 8.76142 11 6C11 3.23858 8.76142 1 6 1C3.23858 1 1 3.23858 1 6C1 8.76142 3.23858 11 6 11Z"
-                      stroke="#555555"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                    <path
-                      d="M6 3V6L8 7"
-                      stroke="#555555"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
+                  <img src="/images/svgicons/clock.svg" alt="clock" />
                   <p>30분</p>
                 </div>
               </div>
