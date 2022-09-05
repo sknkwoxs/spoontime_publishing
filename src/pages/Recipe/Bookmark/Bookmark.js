2d35d5797ad255cf0c512d582ad43dfc36615eb7
seankim1111 <yongsun199@gmail.com>
2022-09-05 12:25:58 +0900

    add Bookmark page

--- src/pages/Recipe/Bookmark/Bookmark.js
+++ src/pages/Recipe/Bookmark/Bookmark.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 export default function Bookmark() {
   return (
     <>
-      <article className="px-4">
+      <article className="h-full px-4">
         <div className="flex py-4">
-          <div className="absolute">
+          <div>
             <Link to="/Recipe">
               <img
                 src="/images/svgIcons/directionsLeft.svg"
@@ -15,22 +15,31 @@ export default function Bookmark() {
           </div>
           <p className="mx-auto Font16sb">즐겨 찾기</p>
         </div>
-        <div>
-          <div className="flex w-full py-2 border-b Font14sb">
+        <div className="h-full">
+          <div className="flex w-full py-2 border-b Font14sb text-GreyScale-grey03">
             {/* tab */}
             <div className="flex justify-center w-full text-center">레시피</div>
             <div className="flex justify-center w-full text-center">식재료</div>
           </div>
-          <div>
-            <div>사진</div>
+          <div className="absolute top-0 bottom-0 left-0 right-0 flex flex-col justify-center mx-4">
+            <div className="mb-4">
+              <img
+                className="object-cover h-[128px] w-full"
+                src="/images/RecipeDetails1.jpg"
+                alt="RecipeDetails1"
+              />
+            </div>
             <div>
-              <p className="mb-4 text-center">
+              <p className="mb-4 text-center Font14">
                 즐겨 찾는 레시피가 없습니다.
                 <br /> 마음에 드는 레시피에 (하트)를 눌러보세요.
               </p>
               <button className="flex justify-center px-8 py-2 mx-auto mt-5 border rounded Font14sb border-BrandColor-green01">
                 레시피 보러 가기
               </button>
+              {/* <button className="flex justify-center px-8 py-2 mx-auto mt-5 border rounded Font14sb border-BrandColor-green01">
+                식재료 보러 가기
+              </button> */}
             </div>
           </div>
         </div>
