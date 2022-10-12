5dba4adb7034208007978e6e06385dfb2592a8a0
seankim1111 <yongsun199@gmail.com>
2022-10-12 16:18:08 +0900

    modify Login page 배경색상 fix 및 font weight 변경

--- src/pages/Login/PrivacyPolicy.js
+++ src/pages/Login/PrivacyPolicy.js
@@ -3,8 +3,8 @@ export function PrivacyPolicy() {
     <>
       <div>
         <p className="mt-2 text-center Font12">
-          로그인하시면 스푼타임의 <span className="Font12sb">이용약관</span>{" "}
-          및&nbsp;
+          로그인하시면 스푼타임의 <span className="Font12sb">이용약관</span>
+          &nbsp;및&nbsp;
           <span className="Font12sb">개인정보처리방침</span>에
           <br /> 동의하는 것으로 간주합니다.
         </p>
