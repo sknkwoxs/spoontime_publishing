47b702eb4ba35b3ea62c2b4dfe6f2d527b9f3409
seankim1111 <yongsun199@gmail.com>
2022-10-14 10:05:47 +0900

    test Calendar api test

--- src/pages/MealPlan/Calendar/Calendar.js
+++ src/pages/MealPlan/Calendar/Calendar.js
@@ -1,15 +1,43 @@
 import koLocale from "@fullcalendar/core/locales/ko";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { formatDate } from "@fullcalendar/common";
 
 export default function Calendar() {
+  const handleDateClick = (arg) => {
+    alert(arg.dateStr);
+  };
+
+  let str = formatDate(new Date(), {
+    month: "long",
+    year: "numeric",
+    day: "numeric",
+  });
+
+  console.log(str);
+
+  console.log();
+
   return (
     <>
       <FullCalendar
         locale={koLocale}
-        plugins={[dayGridPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
+        // weekends={false}
+        dateClick={handleDateClick}
+        eventContent={renderEventContent}
       />
+    </>
+  );
+}
+
+function renderEventContent(eventInfo) {
+  return (
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
     </>
   );
 }
