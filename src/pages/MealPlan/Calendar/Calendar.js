a0dd710449ef9324097b24cfcf32a08e50d1f74d
seankim1111 <yongsun199@gmail.com>
2022-10-17 11:16:23 +0900

    add fullcalendar-vars.css 파일로 css 제어 중

--- src/pages/MealPlan/Calendar/Calendar.js
+++ src/pages/MealPlan/Calendar/Calendar.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import koLocale from "@fullcalendar/core/locales/ko";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
@@ -9,13 +10,13 @@ export default function Calendar() {
     alert(arg.dateStr);
   };
 
-  let str = formatDate(new Date(), {
-    month: "long",
-    year: "numeric",
-    day: "numeric",
-  });
+  // let str = formatDate(new Date(), {
+  //   month: "long",
+  //   year: "numeric",
+  //   day: "numeric",
+  // });
 
-  console.log(str);
+  // console.log(str);
 
   console.log();
 
@@ -23,22 +24,25 @@ export default function Calendar() {
     <>
       <FullCalendar
         locale={koLocale}
+        // dateFormatter={(locale, date) =>
+        //   date.toLocaleString("en", { day: "numeric" })
+        // }
         plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
         // weekends={false}
         // dateClick={handleDateClick}
-        eventContent={renderEventContent}
+        // eventContent={renderEventContent}
         showNonCurrentDates={false}
       />
     </>
   );
 }
 
-function renderEventContent(eventInfo) {
-  return (
-    <>
-      <b>{eventInfo.timeText}</b>
-      <i>{eventInfo.event.title}</i>
-    </>
-  );
-}
+// function renderEventContent(eventInfo) {
+//   return (
+//     <>
+//       <b>{eventInfo.timeText}</b>
+//       <i>{eventInfo.event.title}</i>
+//     </>
+//   );
+// }
