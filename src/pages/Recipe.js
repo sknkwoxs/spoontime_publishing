e9a05402a02627759a7ff280ad37168da6223f40
seankim1111 <yongsun199@gmail.com>
2022-10-24 21:36:14 +0900

    modify Filter page css 수정

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -26,8 +26,8 @@ export default function Recipe() {
       </div>
       {/* <Alert /> */}
       {/* <PopUp /> */}
-      {/* <Filter /> */}
-      <RecipeSort />
+      <Filter />
+      {/* <RecipeSort /> */}
     </>
   );
 }
