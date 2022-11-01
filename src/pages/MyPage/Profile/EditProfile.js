30c880f899eb9135534479dbb00889f23cb54863
seankim1111 <yongsun199@gmail.com>
2022-11-01 16:44:42 +0900

    add MyPage Delete modal & Edit Pic modal에 외부 화면 스크롤 방지

--- src/pages/MyPage/Profile/EditProfile.js
+++ src/pages/MyPage/Profile/EditProfile.js
@@ -1,4 +1,20 @@
+import { useEffect } from "react";
+
 export default function EditProfile() {
+  // 외부 화면 스크롤 방지
+  useEffect(() => {
+    document.body.style.cssText = `
+            position: fixed; 
+            top: -${window.scrollY}px;
+            overflow-y: scroll;
+            width: 100%;`;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = "";
+      window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
+    };
+  }, []);
+
   return (
     <>
       <div className="fixed top-0 left-0 right-0 w-full h-full bg-[#00000099] z-[999]"></div>
