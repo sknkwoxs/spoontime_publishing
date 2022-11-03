6595be1447c04adf2a06ceea1d1302cb0ec5fb9e
seankim1111 <yongsun199@gmail.com>
2022-11-03 16:43:48 +0900

    modify Splash page 레이아웃 및 css 수정 중

--- src/pages/Splash/Splash.js
+++ src/pages/Splash/Splash.js
@@ -29,21 +29,21 @@ export default function Splash() {
       title2: "내 아기 맞춤형",
       bold: " 레시피 추천",
       src: "/images/defaultImage.png",
-      alt: "defaultImage",
+      alt: "SplashDefault",
     },
     {
       title1: "맞춤형 식단까지 알아서 척척",
       title2: "걱정 없는",
       bold: " 식단 관리",
       src: "/images/defaultImage.png",
-      alt: "defaultImage",
+      alt: "SplashDefault",
     },
     {
       title1: "예쁘게 기록하고",
       title2: "쉽게 공유하는",
       bold: " 식단 후기",
       src: "/images/defaultImage.png",
-      alt: "defaultImage",
+      alt: "SplashDefault",
     },
     {
       title1: "우리 아기 밥상 고민",
@@ -51,42 +51,37 @@ export default function Splash() {
       bold: "스푼타임",
       title3: "으로 쉽고 편리하게",
       src: "/images/defaultImage.png",
-      alt: "defaultImage",
+      alt: "SplashDefault",
     },
   ];
 
   return (
     <>
-      <div className="relative">
+      <div className="">
         <Slider
           {...settings}
-          style={{ display: "flex", flexDirection: "column-reverse" }}
+          style={{
+            display: "flex",
+            height: "100%",
+            flexDirection: "column-reverse",
+          }}
         >
           {contents.map((contents, index) => {
             return (
               <li className="" key={index}>
-                <div className="text-center Font24">{contents.title1}</div>
-                <div className="text-center Font24 mb-14">
-                  {contents.title2}
-                  <span className="Font24sb">{contents.bold}</span>
-                  <span className="text-center Font24">{contents.title3}</span>
-                </div>
-                {/* <div className="">
-                  <div className="h-0 pb-[45%] overflow-hidden relative">
-                    <img
-                      className="absolute object-cover w-full h-full"
-                      src={contents.src}
-                      alt={contents.alt}
-                    />
+                <div>
+                  <div className="text-center Font24">{contents.title1}</div>
+                  <div className="text-center Font24 mb-14">
+                    {contents.title2}
+                    <span className="Font24sb">{contents.bold}</span>
+                    <span className="text-center Font24">
+                      {contents.title3}
+                    </span>
                   </div>
-                </div> */}
+                </div>
 
-                <div className="w-[248px] h-[552px] mx-auto drop-shadow-splash">
-                  <img
-                    className="object-cover w-full h-full"
-                    src={contents.src}
-                    alt={contents.alt}
-                  />
+                <div className="flex drop-shadow-splash">
+                  <img className="" src={contents.src} alt={contents.alt} />
                 </div>
               </li>
             );
